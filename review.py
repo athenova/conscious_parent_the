61d7759f97ef39bc8aedd9fb80dc06416095bc5b
athenova <aleksey.sergeyev@yandex.com>
2025-02-22 13:52:07 +0300

    Рефакторинг, добавление интеллекта

---
+++ review.py
@@ -0,0 +1,5 @@
+from send import job
+
+CHAT_ID = -1002374309134
+
+job(CHAT_ID=CHAT_ID)
--- send.py
+++ send.py
@@ -4,39 +4,29 @@ import schedule
 import time
 from openai import OpenAI
 
-#AI_TEXT_MODEL = 'chatgpt-4o-latest'
-AI_TEXT_MODEL = 'gpt-4o-mini'
+AI_TEXT_MODEL = 'chatgpt-4o-latest'
 BOT_TOKEN_NAME = "ATHE_BOT_TOKEN"
 BOT_TOKEN = os.environ.get(BOT_TOKEN_NAME)
 # CHAT_ID = -1002374309134
 CHAT_ID = '@conscious_mother_the'
 
-def job(parent, child):
+def job(CHAT_ID=CHAT_ID):
     client = OpenAI()
     text = client.chat.completions.create(
         model=AI_TEXT_MODEL,
         messages=[
-            { "role": "developer", "content": parent },
-            { "role": "user", "content": child },
+            { "role": "developer", "content": f"Ты - осознанная мать, ты много раз ошибалась в воспитании детей, но теперь хочешь восстановить хорошие отношения" },
+            { "role": "user", "content": f"Пожелай хорошего дня дочери, используй смайлики" },
         ]
     ).choices[0].message.content
     bot = telebot.TeleBot(BOT_TOKEN)
     bot.send_message(chat_id=CHAT_ID, text=text, parse_mode="Markdown")
 
-mother = f"Ты - осознанная мать, ты много раз ошибалась в воспитании детей, но теперь хочешь восстановить хорошие отношения"
-#father = f"Ты - осознанный отец, ты много раз ошибался в воспитании детей, но теперь хочешь восстановить хорошие отношения"
-daughter = f"Пожелай хорошего дня дочери, используй смайлики"
-#son = f"Пожелай хорошего дня сыну, используй смайлики"
+if __name__ == '__main__':
+    schedule.every().day.at("07:00",'Europe/Moscow').do(job)
 
-#job(mother, daughter)
-#job(father, daughter)
-#job(mother, son)
-#job(father, son)
+    fifteen_minutes = 15 * 60
 
-schedule.every().day.at("07:00",'Europe/Moscow').do(job, parent=mother, child=daughter)
-
-fifteen_minutes = 15 * 60
-
-for i in range(fifteen_minutes):
-    schedule.run_pending()
-    time.sleep(1)
+    for i in range(fifteen_minutes):
+        schedule.run_pending()
+        time.sleep(1)
