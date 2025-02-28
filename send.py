d27622dd7e774399dd3664684ce13fc32e95a063
athenova <aleksey.sergeyev@yandex.com>
2025-02-28 21:20:49 +0300

    Переход на DeepSeek

--- send.py
+++ send.py
@@ -4,19 +4,20 @@ import schedule
 import time
 from openai import OpenAI
 
-AI_TEXT_MODEL = 'chatgpt-4o-latest'
+AI_TEXT_MODEL = 'deepseek-chat'
+CHAT_TOKEN_NAME = "DEEPSEEK_API_KEY"
 BOT_TOKEN_NAME = "ATHE_BOT_TOKEN"
 BOT_TOKEN = os.environ.get(BOT_TOKEN_NAME)
 # CHAT_ID = -1002374309134
 CHAT_ID = '@conscious_mother_the'
 
 def job(CHAT_ID=CHAT_ID):
-    client = OpenAI()
+    client = OpenAI(api_key=os.environ.get(CHAT_TOKEN_NAME), base_url="https://api.deepseek.com")
     text = client.chat.completions.create(
         model=AI_TEXT_MODEL,
         messages=[
-            { "role": "developer", "content": f"Ты - осознанная мать, ты много раз ошибалась в воспитании детей, но теперь хочешь восстановить хорошие отношения" },
-            { "role": "user", "content": f"Пожелай хорошего дня дочери, используй смайлики" },
+            { "role": "system", "content": f"Ты - осознанная мать, ты много раз ошибалась в воспитании детей, но теперь хочешь восстановить хорошие отношения" },
+            { "role": "user", "content": f"Пожелай хорошего дня дочери, используй смайлики, не используй 'Конечно'" },
         ]
     ).choices[0].message.content
     bot = telebot.TeleBot(BOT_TOKEN)
